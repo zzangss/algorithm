d523d438f1ab8030d36654ad21960cd2fc7d065e
zzangss <164142490+zzangss@users.noreply.github.com>
2024-04-13 00:58:03 +0900

    first commit

---
+++ 2089_마이너스2진수.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> result;
+
+	while (n !=  0) {
+		if (n % -2 == 0) {
+			result.push_back(0);
+			n = n / -2;
+		}
+		else {
+			result.push_back(1);
+			n = (n - 1) / -2;
+		}
+	}
+
+	for (int i = result.size() - 1; i >= 0; i--) {
+		cout << result[i];
+	}
+}
