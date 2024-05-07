ddc3f24e41cec0faaf4fb9bbe302d45c6237a27e
zzangss <164142490+zzangss@users.noreply.github.com>
2024-05-08 02:02:02 +0900

    second commit

--- 11576_Base_Conversion.cpp
+++ 11576_Base_Conversion.cpp
@@ -1,39 +1,17 @@
 #include<iostream>
 #include<vector>
+#include<stack>
+#include<math.h>
 
 using namespace std;
 
-int aToTen(vector<int> v, int a) {
+stack <int> tenToB(int ten, int b) {
+	stack<int> result;
 
-	int tmp = 1;
-	int result = 0;
-	for (int i = v.size()-1; i >= 0; i--) {
-		result += v[i] * tmp;
-		tmp *= a;
-	}
-
-	return result;
-}
-
-vector<int> tenToB(int ten, int b) {
-	vector<int> tmp;
-	vector<int>result;
-
-	int 몫 = ten;
-	int 나머지 = 0;
-	while (true) {
-		if (몫 < b) {
-			break;
-		}
-		나머지 = 몫 % b;
-		몫 /= b;
-
-		tmp.push_back(나머지);
-	}
-	tmp.push_back(몫);
-
-	for (int i = tmp.size() - 1; i >= 0; i--) {
-		result.push_back(tmp[i]);
+	int tmp = ten;
+	while (tmp) {
+		result.push(tmp % b);
+		tmp /= b;
 	}
 
 	return result;
@@ -44,24 +22,25 @@ int main() {
 	cin >> a >> b;
 	cin >> num;
 
-	vector<int> v(num,0);
-
-	for (int i = 0; i < num; i++) {
-		int tmp;
-		cin >> tmp;
-		v[i] = tmp;
-	}
-
 	int ten = 0;
 
-	ten = aToTen(v, a);
+
+	while(num--) {
+		int tmp;
+		cin >> tmp;
+		ten += tmp * pow(a, num);
+	}
+	//입력과 동시에 10진수로 변환 가능하다.
+	//다른 진법 변환 문제와 달리 10 이상의 숫자가
+	//정수 그대로 표현되기 때문이다.
 	
-	vector<int>result;
+	stack<int>result;
 
 	result = tenToB(ten, b);
-
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << " ";
+	int size = result.size();
+	for (int i = 0; i < size; i++) {
+		cout << result.top() << " ";
+		result.pop();
 	}
 
 }
