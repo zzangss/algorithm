4cbf3340b77a21db7bed2635140b23fd52144c0e
zzangss <164142490+zzangss@users.noreply.github.com>
2024-05-13 21:58:04 +0900

    second commit

--- 2531_회전초밥.cpp
+++ 2531_회전초밥.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
@@ -19,25 +18,37 @@ int main() {
 	int s = 0;
 	int e = k - 1; //1~k 슬라이싱
 
-	int kind = 0, MAX = 0;
-
-	check[c]++;
-	kind++;
+	int kind = 0;
+	int max = 0;
+	int checkC = 0;
 
 	for (int i = 0; i < k; i++) {
 		check[v[i]]++;
+		if (v[i] == c) {
+			checkC++;
+		}
 		if (check[v[i]] == 1) {
 			kind++;
 		}
+
 	}
 
-	MAX = kind;
+	max = kind;
 
-	while (s<n) {
+	while (true) {
+		if (s >= n - 1) {
+			break;
+		}
+		if (kind >= n + 1) {
+			break;
+		}
 
 		check[v[s]]--;
 		if (check[v[s]] == 0) {
 			kind--;
+		}
+		if (v[s] == c) {
+			checkC--;
 		}
 		s++;
 
@@ -51,17 +62,25 @@ int main() {
 		if (check[v[e]] == 1) {
 			kind++;
 		}
+		if (v[e] == c) {
+			checkC++;
+		}
 
-		if (check[c] == 0) {
-			MAX = max(MAX, kind + 1);
+
+		if (checkC >= 1) {
+			if (max < kind) {
+				max = kind;
+			}
 		}
 		else {
-			MAX = max(MAX, kind);
+			if (max < kind + 1) {
+				max = kind + 1;
+			}
 		}
 
 	}
 
-	cout << MAX;
+	cout << max;
 
 	return 0;
 }
