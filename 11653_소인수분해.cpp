d583879882ad89d368f9815347ba08b8d9157eae
zzangss <164142490+zzangss@users.noreply.github.com>
2024-05-13 17:51:39 +0900

    first commit

---
+++ 11653_소인수분해.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		while (n % i == 0) {
+			cout << i << "\n";
+			n /= i;
+		}
+	}
+}
