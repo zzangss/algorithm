18cc6df26b4ae4b80cbe55cb898d52f3e703b5b8
zzangss <164142490+zzangss@users.noreply.github.com>
2024-05-28 23:41:20 +0900

    second commit

--- 12904_A와B.cpp
+++ 12904_A와B.cpp
@@ -4,24 +4,27 @@
 
 using namespace std;
 
-int greedy(string s1, string s2) {
-	if (s1 == s2) return 1;
-	if (s1.size() > s2.size()) return 0;
-
-	if (s2.find(s1 + "A") != string::npos) {
-		return greedy(s1 + "A", s2);
-	}
-	reverse(s1.begin(), s1.end());
-	if (s2.find(s1 + "B") != string::npos) {
-		return greedy(s1 + "B", s2);
-	}
-
-	return 0;
-}
 int main() {
 	string str1, str2;
 	cin >> str1 >> str2;
 
-	int result = greedy(str1, str2);
-	cout << result;
+	int size = str2.size();
+	int answer = 0;
+	while (true) {
+		if (str1 == str2) { 
+			answer = 1;
+			break;
+		}
+		if (str2.size() == 0) break;
+
+		if (str2[str2.size() - 1] == 'A') {
+			str2 = str2.substr(0, str2.size()-1);
+		}
+		else {
+			str2 = str2.substr(0, str2.size() - 1);
+			reverse(str2.begin(), str2.end());
+		}
+	}
+
+	cout << answer;
 }
