4f210ea4c9129dc0afc7c2affe7f08098df643e5
zzangss <164142490+zzangss@users.noreply.github.com>
2024-08-09 22:28:49 +0900

    Update 15649_N과M1.cpp

--- 15649_N과M1.cpp
+++ 15649_N과M1.cpp
@@ -1,27 +1,9 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int arr[9];
-bool visited[9] = { false , };
-
-void dfs(int n, int m, int d) {
-	if (d == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
-		}
-		cout << '\n';
-		return;
-	}
-	for (int i = 1; i <= n; i++) {
-		if (!visited[i]) {
-			visited[i] = true;
-			arr[d] = i;
-			dfs(n, m, d + 1);
-			visited[i] = false;
-		}
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -30,7 +12,31 @@ int main() {
 
 	int n, m; cin >> n >> m;
 
-	dfs(n, m, 0);
+	vector<int> v(n);
+	vector<int> temp(m);
+	int check;
+
+	for (int i = 0; i < n; i++) {
+		v[i] = i + 1;
+	}
+	
+	do {
+		check = true;
+		for (int i = 0; i < m; i++) {
+			if (temp[i] != v[i]) {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			continue;
+		}
+		for (int i = 0; i < m; i++) {
+			cout << v[i] << " ";
+			temp[i] = v[i];
+		}
+		cout << "\n";
+	} while (next_permutation(v.begin(), v.end()));
 
 	return 0;
 }
