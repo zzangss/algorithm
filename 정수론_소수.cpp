0c4bb8723f94f0eaabde3146d85afea7133f1e15
zzangss <164142490+zzangss@users.noreply.github.com>
2024-09-04 14:12:30 +0900

    Create 정수론_소수.cpp

---
+++ 정수론_소수.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+vector<bool> findPrime(int n) {
+	vector<bool> is_prime(n + 1, true);
+
+	for (int i = 2; i <= sqrt(n); i++) {
+		for (int k = i * i; k <= n; k += i) {
+			if (is_prime[k] == false) continue;
+			is_prime[k] = false;
+		}
+	}
+
+	return is_prime;
+}
+
+int main() {
+	int n; cin >> n;
+
+	vector<bool> is_prime= findPrime(n);
+
+	for (int i = 2; i <= n; i++) {
+		if (is_prime[i]) cout << i << " ";
+	}
+
+	return 0;
+}
